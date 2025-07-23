36e4cc0445138885b021df606f8b1ece75bc6385
daniel.becon <daniel.becon@premiumtrustbank.com>
2025-07-23 05:18:20 +0100

    Updated the Server

--- Src/server.ts
+++ Src/server.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from './app';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
