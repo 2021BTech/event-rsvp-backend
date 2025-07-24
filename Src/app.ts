425d670cd8e90d0cd8b3b2820e935bdeb706eeb7
daniel.becon <daniel.becon@premiumtrustbank.com>
2025-07-24 01:08:31 +0100

    updated the backend space

--- Src/app.ts
+++ Src/app.ts
@@ -16,7 +16,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 // Swagger route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
